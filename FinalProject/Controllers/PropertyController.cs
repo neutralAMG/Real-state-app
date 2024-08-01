1dc05e1b26897595113b3bcfc6d24862a1332c95
IamNestor <160444238+IamNestor@users.noreply.github.com>
2024-08-01 14:14:55 -0400

    Cosas de Front

---
+++ FinalProject/Controllers/PropertyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chequeando.Controllers
+{
+    public class PropertyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalProject/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using FinalProject.Core.Application.Dtos.Identity.Account;
+using FinalProject.Core.Application.Interfaces.Contracts.Identity;
+using FinalProject.Core.Domain.Settings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Presentation.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IAccountService accountService;
+
+        //public UserController(IAccountService _accountService)
+        //{
+        //    accountService = _accountService;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Login(string nameormail, string password)
+        //{
+        //    await accountService.AuthenticateWebAppAsync(nameormail, password);
+        //    return View();
+        //}
+    }
+}
