dad817faa46a9eda2860d7ab54defcc5b5651d8e
IamNestor <160444238+IamNestor@users.noreply.github.com>
2024-08-03 12:41:39 -0400

    arreglando unos errores

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -45,16 +45,6 @@ namespace Chequeando.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
